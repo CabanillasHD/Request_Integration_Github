daf048ef372cddb34de6119284736bb66553c03d
Henrique Cabanillas <cabanillas445@gmail.com>
2022-10-03 21:16:05 -0300

    Fix bug - Empety parameters

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-from modulos import integra_git, imprimi_format
+from modulos import integra_git, imprimi_format, ledados
 
-nome = str(input('Digite o nome do seu GITHUB: ')).strip()
-projeto = str(input('Digite o nome do seu Projeto: ')).strip()
+nome, projeto = ledados()
+num_estrelas = integra_git(nome, projeto)
 
-if nome.strip() == '':
-    num_estrelas = integra_git()
-elif projeto.strip() == '':
-    num_estrelas = integra_git()
-else:
-    num_estrelas = integra_git(nome, projeto)
 
 if __name__ == "__main__":
     imprimi_format(num_estrelas)
--- modulos.py
+++ modulos.py
@@ -16,7 +16,25 @@ def imprimi_format(num):
         print('Ops: Algo deu errado')
     else:
         print('=*' * 20)
-        print(f'Esse projeto tem um total de {estrelas} estrelas' if estrelas > 1 else
-              f'Esse projeto tem um total de {estrelas} estrela')
+        print(f'Esse projeto tem um total de: {estrelas} estrelas' if estrelas > 1 else
+              f'Esse projeto tem um total de: {estrelas} estrela')
         print('=*' * 20)
 
+
+def ledados():
+    git = str(input('Digite o nome do seu GITHUB: ')).strip()
+    if git.strip() == '':
+        git = 'CabanillasHD'
+        proj = 'PythonBeginner'
+        print(f'Você não preencheu o nome do Github, com isso usaremos o {git} e o projeto {proj}, '
+              'como padrão')
+        return git, proj
+    else:
+        gitpreenchido = git
+        proj = str(input('Digite o nome do seu Projeto: ')).strip()
+        if proj.strip() == '':
+            proj = str('PythonBeginner')
+            print(f'Nome de projeto inválido, o git usado será o {git} e projeto {proj} será usado como padrão')
+            return gitpreenchido, proj
+        else:
+            return git, proj
